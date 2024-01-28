4955c8d6b911e657e5e38c6f49d24d01758e8f74
Harsha S <157780689+harshasnayaka@users.noreply.github.com>
2024-01-28 18:36:23 +0530

    Add files via upload

---
+++ Pro 5.py
@@ -0,0 +1,11 @@
+print("Welcome to The Game !")
+num1 = int(input("Enter your First Number : "))
+num2 = int(input("Enter your Second Number : "))
+result = num1 + num2
+print(f"The Sum Of Two Numbers is :{result}")
+if result <100:
+    print("Yes Continue ")
+else :
+    print("Numbers Range Must be Betwwen 1 - 100") 
+print("Thank You")
+
---
+++ pro 3.py
@@ -0,0 +1,7 @@
+name = input("Enter your Name : ")
+print("Hello %s , Welcome to XXX Bank (name)")
+password = int(input("Enter your 5 digit pin : "))
+if password == 5757:
+    print("Welcome to the further transaction ! ")
+else :
+    print("Pin is Invalid ! ")
